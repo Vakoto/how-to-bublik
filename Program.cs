0e32cf5d37b150816edd1cd24f3dec8bdf58e5de
MultiZver <MultiZver@yandex.ru>
2016-07-13 00:31:32 +0500

    hw

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello, world!";
+            Console.WriteLine("{0}", s);
+        }
+    }
+}
